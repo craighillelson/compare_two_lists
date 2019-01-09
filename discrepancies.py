095a47454e6d06b3cac190084b93019dab0bc358
Craig <craig.hillelson@intersection.com>
2019-01-09 13:30:01 -0500

    first upload

---
+++ discrepancies.py
@@ -0,0 +1,27 @@
+""" __doc__ """
+
+LST1 = []
+LST2 = []
+
+def import_text_file(text_file, lst):
+    """ import text file and throw the contents into a list """
+    users = open(text_file, "r")
+    for email in users:
+        lst.append(email)
+
+# run import_text_file for both files
+import_text_file('users_1.txt', LST1)
+import_text_file('users_2.txt', LST2)
+
+# find discrepancies and concatenate both lists of same
+LST1_LST2 = set(LST1) - set(LST2)
+LST2_LIST1 = set(LST2) - set(LST1)
+DISCREPANCIES = list(LST1_LST2) + list(LST2_LIST1)
+
+# output file
+DISCREPANCIES_FILE = open("discrepancies.txt", "w")
+for user in DISCREPANCIES:
+    DISCREPANCIES_FILE.write(user+"\n")
+
+# update user
+print "'discrepancies.txt' exported successfully"
