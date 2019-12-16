7e9ffc873dfffa441ceaf2cce4bc701cb9b7cd5e
Craig Hillelson <craighillelson@gmail.com>
2019-12-16 08:36:26 -0500

    updated to single quotes

--- discrepancies.py
+++ discrepancies.py
@@ -5,7 +5,7 @@ LST2 = []
 
 def import_text_file(text_file, lst):
     """ import text file and throw the contents into a list """
-    users = open(text_file, "r")
+    users = open(text_file, 'r')
     users = [lst.append(email) for email in users]
 
 # run import_text_file for both files
@@ -18,9 +18,9 @@ LST2_LIST1 = set(LST2) - set(LST1)
 DISCREPANCIES = list(LST1_LST2) + list(LST2_LIST1)
 
 # output file
-DISCREPANCIES_FILE = open("discrepancies.txt", "w")
+DISCREPANCIES_FILE = open('discrepancies.txt', 'w')
 for user in DISCREPANCIES:
     DISCREPANCIES_FILE.write(user)
 
 # update user
-print("'discrepancies.txt' exported successfully")
+print('"discrepancies.txt" exported successfully')
