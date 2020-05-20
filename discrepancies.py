b1505dd59e2790980827832e42a4c3e9986637ec
Craig Hillelson <craighillelson@gmail.com>
2020-05-20 10:30:50 -0400

    update discrepancies.py
    made code more modular

--- discrepancies.py
+++ discrepancies.py
@@ -1,6 +1,7 @@
 """
-Compare two lists and output a text file with the discrepancies between the
-two.
+Compare two lists.
+
+Output a text file with the discrepancies between the two.
 """
 
 LST1 = []
@@ -9,6 +10,7 @@ LST2 = []
 
 def import_text_file(text_file, lst):
     """Import a text file and populate a list with its contents."""
+    
     users = open(text_file, 'r')
     users = [lst.append(email) for email in users]
 
@@ -20,19 +22,24 @@ def update_user():
     print('"discrepancies.txt" exported successfully')
 
 
-# run import_text_file for both files
+def compare_sets(a, b):
+    """Compare sets."""
+
+    return set(a) - set(b)
+
+
+def write_discreps_to_file():
+    """Write discrepancies to a text file."""
+
+    DISCREPANCIES_FILE = open('discrepancies.txt', 'w')
+    for user in DISCREPANCIES:
+        DISCREPANCIES_FILE.write(user)
+
+
 import_text_file('users_1.txt', LST1)
 import_text_file('users_2.txt', LST2)
-
-# find discrepancies and concatenate both lists of same
-LST1_LST2 = set(LST1) - set(LST2)
-LST2_LIST1 = set(LST2) - set(LST1)
+LST1_LST2 = compare_sets(LST1, LST2)
+LST2_LIST1 = compare_sets(LST2, LST1)
 DISCREPANCIES = list(LST1_LST2) + list(LST2_LIST1)
-
-# output file
-DISCREPANCIES_FILE = open('discrepancies.txt', 'w')
-for user in DISCREPANCIES:
-    DISCREPANCIES_FILE.write(user)
-
-# update user
+write_discreps_to_file()
 update_user()
