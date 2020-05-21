d5dd3e9f978c8c0077ea2bde9db08dd857fd00a7
Craig Hillelson <craighillelson@gmail.com>
2020-05-21 07:33:52 -0400

    update discrepancies.py
    "lst" is now defined inside the import_text_file function, making code a little more readable

--- discrepancies.py
+++ discrepancies.py
@@ -4,15 +4,13 @@ Compare two lists.
 Output a text file with the discrepancies between the two.
 """
 
-LST1 = []
-LST2 = []
 
-
-def import_text_file(text_file, lst):
+def import_text_file(text_file):
     """Import a text file and populate a list with its contents."""
-    
-    users = open(text_file, 'r')
-    users = [lst.append(email) for email in users]
+
+    lst = []
+    lst = open(text_file).readlines()
+    return lst
 
 
 def update_user():
@@ -36,8 +34,8 @@ def write_discreps_to_file():
         DISCREPANCIES_FILE.write(user)
 
 
-import_text_file('users_1.txt', LST1)
-import_text_file('users_2.txt', LST2)
+LST1 = import_text_file('users_1.txt')
+LST2 = import_text_file('users_2.txt')
 LST1_LST2 = compare_sets(LST1, LST2)
 LST2_LIST1 = compare_sets(LST2, LST1)
 DISCREPANCIES = list(LST1_LST2) + list(LST2_LIST1)
