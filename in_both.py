77e9b266247e9d4608bbeea6bcf2d6b6e853d111
Craig Hillelson <craighillelson@gmail.com>
2020-05-23 10:34:17 -0400

    update in_both.py
    simplified code

--- in_both.py
+++ in_both.py
@@ -1,24 +1,22 @@
-"""Compare the contents of two lists. Output the items common to both."""
+"""Compare two lists. Write the values in both lists to a file."""
 
-LST1 = []
-LST2 = []
 
-def import_text_file(text_file, lst):
-    """ import text file and throw the contents into a list """
-    users = open(text_file, "r")
-    users = [lst.append(email) for email in users]
+def import_text_file(text_file):
+    """Import text file and throw the contents into a list."""
+    lst = []
+    lst = open(text_file).readlines()
+    return lst
 
-# import text
-import_text_file('users_1.txt', LST1)
-import_text_file('users_2.txt', LST2)
 
-# find values in both lists
+LST1 = import_text_file('users_1.txt')
+LST2 = import_text_file('users_2.txt')
+
 IN_BOTH = set(LST1) & set(LST2)
 
-# output file
-IN_BOTH_FILE = open("in_both.txt", "w")
-for user in IN_BOTH:
-    IN_BOTH_FILE.write(user)
-
-# update user
-print("'in_both.txt' exported successfully")
+if IN_BOTH:
+    IN_BOTH_FILE = open('in_both.txt', 'w')
+    for user in IN_BOTH:
+        IN_BOTH_FILE.write(user)
+    print("'in_both.txt' exported successfully")
+else:
+    print('There are no items in common.')
