b9cbd823abb9d2dea6be7f5244dbab08392797d4
Craig Hillelson <craighillelson@gmail.com>
2020-10-08 19:51:31 -0400

    update in_both.py
    linted code and changed string quoting to be consistent with other repos

--- in_both.py
+++ in_both.py
@@ -3,19 +3,18 @@
 
 def import_text_file(text_file):
     """Import text file and throw the contents into a list."""
-    lst = []
     return open(text_file).readlines()
 
 
-LST1 = import_text_file('users_1.txt')
-LST2 = import_text_file('users_2.txt')
+LST1 = import_text_file("users_1.txt")
+LST2 = import_text_file("users_2.txt")
 
 IN_BOTH = set(LST1) & set(LST2)
 
 if IN_BOTH:
-    IN_BOTH_FILE = open('in_both.txt', 'w')
+    IN_BOTH_FILE = open("in_both.txt", "w")
     for user in IN_BOTH:
         IN_BOTH_FILE.write(user)
     print("'in_both.txt' exported successfully")
 else:
-    print('There are no items in common.')
+    print("There are no items in common.")
