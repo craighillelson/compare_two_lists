c94dde9d180ccf411b3265eefbb32ec285dd84cb
Craig Hillelson <craighillelson@gmail.com>
2020-10-09 00:29:51 -0400

    update discrepancies.py
    linted code and made adjustments to conform to PEP8 standards

--- discrepancies.py
+++ discrepancies.py
@@ -6,29 +6,29 @@ Output a text file with the discrepancies between the two.
 
 def import_text_file(text_file):
     """Import a text file and populate a list with its contents."""
-    lst = []
     return open(text_file).readlines()
 
 
-def compare_sets(a, b):
+def compare_sets(lst_1, lst_2):
     """Compare sets."""
-    return set(a) - set(b)
+    return set(lst_1) - set(lst_2)
 
 
 def find_discrepancies():
     """Write discrepancies to a text file."""
     if DISCREPANCIES:
-        DISCREPANCIES_FILE = open('discrepancies.txt', 'w')
+        discrepancies_file = open("discrepancies.txt", "w")
         for user in DISCREPANCIES:
-            DISCREPANCIES_FILE.write(user)
+            discrepancies_file.write(user)
         print('"discrepancies.txt" exported successfully')
     else:
         print('There are no discrepancies.')
 
 
-LST1 = import_text_file('users_1.txt')
-LST2 = import_text_file('users_2.txt')
+LST1 = import_text_file("users_1.txt")
+LST2 = import_text_file("users_2.txt")
 LST1_LST2 = compare_sets(LST1, LST2)
 LST2_LIST1 = compare_sets(LST2, LST1)
 DISCREPANCIES = list(LST1_LST2) + list(LST2_LIST1)
 find_discrepancies()
+
